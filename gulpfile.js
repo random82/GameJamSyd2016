ca7c808e3a6845a505266688f5cdb7f667cf4423
Dominik Raniszewski <dominik.raniszewski@outlook.com>
2016-01-29 21:00:16 +1100

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+﻿/// <binding Clean='clean' />
+"use strict";
+
+var gulp = require("gulp"),
+    rimraf = require("rimraf"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+var paths = {
+    webroot: "./www/"
+};
+
+paths.icon = paths.webroot + "icon/**/*.*";
+paths.lib = paths.webroot + "lib/**/*.*";
+paths.asset = paths.webroot + "asset/**/*.*";
+paths.src = paths.webroot + "src/**/*.js";
+paths.minSrc = paths.webroot + "src/**/*.min.js";
+paths.css = paths.webroot + "css/**/*.css";
+paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatCssDest = paths.webroot + "css/site.min.css";
+
+gulp.task("clean:js", function (cb) {
+    rimraf(paths.concatJsDest, cb);
+});
+
+gulp.task("clean:css", function (cb) {
+    rimraf(paths.concatCssDest, cb);
+});
+
+gulp.task("clean", ["clean:js", "clean:css"]);
+
+gulp.task("min:src", function () {
+    return gulp.src([paths.src, "!" + paths.minSrc], { base: "." })
+        .pipe(concat(paths.concatJsDest))
+        .pipe(uglify())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min:css", function () {
+    return gulp.src([paths.css, "!" + paths.minCss])
+        .pipe(concat(paths.concatCssDest))
+        .pipe(cssmin())
+        .pipe(gulp.dest("."));
+});
+
+gulp.task("min", ["min:src", "min:css"]);
